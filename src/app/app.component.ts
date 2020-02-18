8671d82811e58e124525f3a12fe3ffc072aaab1f
Pablo Buisson <pablo.buisson@gmail.com>
2020-02-18 11:12:22 +0100

    add pokemon Object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 // import { nom élément } from 'nom paquet source'
 // import { Component } obligatoire
+import { Pokemon } from './pokemon' // ++
 
 @Component({
     selector: 'pokemon-app', // obligatoire : donne un nom au composant
     // correspondant à <pokemon-app></pokemon-app>
     template: `<h1>Hello {{name}}</h1>`, // obligatoire : définit le code HTML
 })
-export class AppComponent { name = 'Angular'; }
+export class AppComponent { 
+    name = 'Angular';
+    // propriété privée, qui renvoit un tableau d'objet de type Pokemon
+    private pokemons: Pokemon[]; 
+}
 // le code de la classe de notre composant
 // contient la logique du composant
 // convention nomcomposantComponent

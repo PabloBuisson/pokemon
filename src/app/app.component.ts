5cd42a3825379a1813d3d3b9490b72c769c187e2
Pablo Buisson <pablo.buisson@gmail.com>
2020-02-18 11:38:53 +0100

    add lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 // import { nom élément } from 'nom paquet source'
 // import { Component } obligatoire
-import { Pokemon } from './pokemon' // ++
+import { OnInit } from '@angular/core'; // ++ (d'abord les angular core)
+
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemon'; // ++
 
 @Component({
     selector: 'pokemon-app', // obligatoire : donne un nom au composant
     // correspondant à <pokemon-app></pokemon-app>
     template: `<h1>Hello {{name}}</h1>`, // obligatoire : définit le code HTML
 })
-export class AppComponent { 
+export class AppComponent implements OnInit { 
     name = 'Angular';
     // propriété privée, qui renvoit un tableau d'objet de type Pokemon
-    private pokemons: Pokemon[]; 
+    private pokemons: Pokemon[];
+
+    ngOnInit() {
+        // étape d'initiliation
+        this.pokemons = POKEMONS;
+    }
 }
 // le code de la classe de notre composant
 // contient la logique du composant

ad1a5262ed3260d535fb525c1f4f8322b5b91dbf
Pablo Buisson <pablo.buisson@gmail.com>
2020-02-19 22:08:58 +0100

    add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 // aide à déclarer les routes de l'application
 import { ListPokemonComponent } from './list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found.component'; // ++
 
 // routes
 const appRoutes: Routes = [
     { path: 'pokemons', component: ListPokemonComponent },
     { path: 'pokemon/:id', component: DetailPokemonComponent },
-    { path: '', redirectTo: 'pokemons', pathMatch: 'full' } // par défaut
+    { path: '', redirectTo: 'pokemons', pathMatch: 'full' }, // par défaut
+    { path: '**', component: PageNotFoundComponent} 
+    // capture toutes les routes pas interceptées précédemment
+    // a mettre en dernier dans le tableau
+    // l'ordre des routes est important : routes précises, puis routes générales
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DetailPokemonComponent } from './detail-pokemon.component'; // ++
-import { ListPokemonComponent } from './list-pokemon.component'; // ++
+import { DetailPokemonComponent } from './detail-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon.component';
 
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { AppRoutingModule } from './app-routing.module'; // ++
+import { AppRoutingModule } from './app-routing.module'; 
+import { PageNotFoundComponent } from './page-not-found.component'; // ++
 
 // permet de déclarer un nouveau module
 @NgModule({
-    imports: [BrowserModule, AppRoutingModule], // ++
+    imports: [BrowserModule, AppRoutingModule],
     declarations: [
         AppComponent, 
         BorderCardDirective, 
         PokemonTypeColorPipe,
-        ListPokemonComponent, // ++
-        DetailPokemonComponent // ++
-    ]
+        ListPokemonComponent,
+        DetailPokemonComponent,
+        PageNotFoundComponent // ++
+    ],
     bootstrap: [AppComponent] // permet d'identifier le composant racine
     // que Angular appelle au démarrage de l'application
 })

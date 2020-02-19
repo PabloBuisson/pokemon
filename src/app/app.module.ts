eb60fa481194d385ca7b50f4b13dc41e8c029000
Pablo Buisson <pablo.buisson@gmail.com>
2020-02-19 09:26:11 +0100

    add customized pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 
-import { BorderCardDirective } from './border-card.directive'; // ++
+import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe'; // ++
 
 // permet de déclarer un nouveau module
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, BorderCardDirective], // ++
+    declarations: [AppComponent, BorderCardDirective, PokemonTypeColorPipe], // ++
     bootstrap: [AppComponent] // permet d'identifier le composant racine
     // que Angular appelle au démarrage de l'application
 })

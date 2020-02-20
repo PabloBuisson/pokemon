5950cc84e19e789f67cbf987e4fd188e1a09bc57
Pablo Buisson <pablo.buisson@gmail.com>
2020-02-20 16:50:46 +0100

    add service

--- src/app/pokemons/list-pokemon.component.ts
+++ src/app/pokemons/list-pokemon.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import { Router } from '@angular/router'; // ++
+import { Router } from '@angular/router';
 
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemons';
+import { PokemonsService } from './pokemons.service';
 
 @Component({
-    selector: 'list-pokemon', // ++
-    templateUrl: './app/pokemons/list-pokemon.component.html', // ++
+    selector: 'list-pokemon',
+    templateUrl: './app/pokemons/list-pokemon.component.html',
+    providers: [PokemonsService] // ++ permet d'accéder au service à l'instanciation
+    // de la classe ListePokemonComponent
 })
 export class ListPokemonComponent implements OnInit {
 
     private pokemons: Pokemon[];
     
-    constructor(private router: Router) { } // ++
+    constructor(private router: Router, private pokemonsService: PokemonsService) {
+        // instance disponible sous forme de propriété privée this.pokemonsService
+        // injection de dépendance garantit que l'instance est unique dans l'appli
+        // si on l'utilise dans un autre composant, ça sera la même instance
+        // comme c'est unique : sert de stockage provisoire de nos données
+        // NE SURTOUT PAS INSTANCIER LE SERVICE DANS LE CONSTRUCTEUR
+        // Un service est une boîte noire qui n'interesse pas les composants
+     }
 
     ngOnInit() {
         // étape d'initiliation

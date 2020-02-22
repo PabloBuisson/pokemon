2003a6fb5b893fc9f28f19ff284ef362fc38b78b
Pablo Buisson <pablo.buisson@gmail.com>
2020-02-22 23:07:53 +0100

    update pokemon service

--- src/app/pokemons/pokemons.service.ts
+++ src/app/pokemons/pokemons.service.ts
@@ -1,13 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs'; // ++
+import { catchError, map, tap } from 'rxjs/operators'; // ++
 
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemons';
 
 @Injectable() // permet d'indiquer que ce service peut avoir des dépendances
-export class PokemonsService { 
-    // Retourne tous les pokémons
-    getPokemons(): Pokemon[] {
-        return POKEMONS;
+export class PokemonsService {
+    
+    // pour créer une url vers laquelle nous allons appeler l'API ++ 
+    private pokemonUrl = 'api/pokemons';
+
+    constructor(private http: HttpClient) { } // ++
+
+    private log(log: string) { // ++
+        console.info(log);
+        // centralise la gestion des logs de notre service
+    }
+
+    // retourne un Observable, qui contient un tableau de pokémons
+    getPokemons(): Observable<Pokemon[]> {
+        // la méthode get de la propriété retourne un Observable de type Pokemon[]
+        // Observable qui envoie une requête HTTP de type GET sur la route api/pokemons
+        // l'opérateur tap interagit sur le déroulement des événements générés par l'Observable
+        // utilisé pour débugagge, archivage de log... 
+        // l'opérateur catchError capte les erreurs
+        // le module renvoie les données sous format JSON
+        return this.http.get<Pokemon[]>(this.pokemonUrl).pipe(
+            tap(() => this.log(`fetched pokemons`)),
+            catchError(this.handleError(`getPokemons`, []))
+        );
     }
 
     // Retourne le pokémon avec l'identifiant passé en paramètre

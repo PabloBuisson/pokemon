4935ad8808bc5148fe40f55ef823a317f884cd08
Pablo Buisson <pablo.buisson@gmail.com>
2020-02-23 17:49:18 +0100

    improve authentication Guard

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot }
+    from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable()
-// service qui implemente l'interface CanActivate
 export class AuthGuard implements CanActivate {
 
-    canActivate(): boolean {
-        console.info(`Le guard a bien été appelé !`);
-        return true;
+    // injecte notre service d'authentification + router
+    constructor(private authService: AuthService, private router: Router) { }
+
+    // l'objet route : la future route qui sera appelée
+    // l'objet state : futur état du routeur de l'appli
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        let url: string = state.url;
+        return this.checkLogin(url);
+    }
+
+    // retourne un booleen de manière synchrone
+    checkLogin(url: string): boolean {
+        if (this.authService.isLoggedIn) { return true; }
+        this.authService.redirectUrl = url; // stocke l'url où l'authentification a échoué
+        this.router.navigate(['/login']); // redirige vers la page de connexion
+
+        return false;
     }
 
 }
